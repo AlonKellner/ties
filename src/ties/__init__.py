a05d34259a24094905dd8b98b4ffd1602d106fa7
Alon Kellner <me@alonkellner.com>
2025-08-18 08:40:47 +0000

    fix: pre-commit passes
    Signed-off-by: Alon Kellner <me@alonkellner.com>

--- src/ties/__init__.py
+++ src/ties/__init__.py
@@ -1,7 +1,7 @@
 """A CLI tool to duplicate and sync file content with advanced transformations."""
 
+from .__about__ import __version__, __version_tuple__
 from ._file_processing import process_files
-from ._version import __commit_id__, __version__, __version_tuple__
 from .common_transforms import embed_environ
 
 __all__ = [

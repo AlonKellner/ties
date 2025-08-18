bcf4c33850a9ffc33beb45920582d0624ed840c9
Alon Kellner <me@alonkellner.com>
2025-08-18 08:44:12 +0000

    fix: simple version tracking
    Signed-off-by: Alon Kellner <me@alonkellner.com>

--- src/ties/__about__.py
+++ src/ties/__about__.py
@@ -1,4 +1,3 @@
 """A tracked and `hatch` managed version."""
 
-__version__ = "0.0.0a7"
-__version_tuple__ = (0, 0, 0, "a7")
+__version__ = "0.0.0a8"
--- src/ties/__init__.py
+++ src/ties/__init__.py
@@ -1,13 +1,11 @@
 """A CLI tool to duplicate and sync file content with advanced transformations."""
 
-from .__about__ import __version__, __version_tuple__
+from .__about__ import __version__
 from ._file_processing import process_files
 from .common_transforms import embed_environ
 
 __all__ = [
-    "__commit_id__",
     "__version__",
-    "__version_tuple__",
     "embed_environ",
     "process_files",
 ]
